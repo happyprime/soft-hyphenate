4b095e1a22daa11bb783acb866883f35f523cd6c
Phil Cable <phil@happyprime.co>
2025-02-20 15:21:30 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Soft Hyphenate
+
+Maintain a library of hyphenation suggestions for long words used throughout your site.
---
+++ plugin.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name:  Soft Hyphenate
+ * Description:  Maintain a library of hyphenation suggestions for long words used throughout your site.
+ * Version:      0.0.1
+ * Plugin URI:   https://github.com/happyprime/soft-hyphenate/
+ * Author:       Happy Prime
+ * Author URI:   https://happyprime.co
+ * Text Domain:  soft-hyphenate
+ * Requires PHP: 8
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @package soft-hyphenate
+ */
+
+namespace SoftHyphenate;
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
